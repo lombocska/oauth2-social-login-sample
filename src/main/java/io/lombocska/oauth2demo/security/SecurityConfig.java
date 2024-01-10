9803bf87ff37ed9de1a6d8b7ca92bdcdfb8bba9e
lombosmonika <lombos.monika@gmail.com>
2024-01-10 13:24:56 +0100

    chore: github, facebook, google oauth2 config

---
+++ src/main/java/io/lombocska/oauth2demo/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package io.lombocska.oauth2demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer debugCustomizer() {
+        return (web) -> web.debug(true);
+    }
+}
